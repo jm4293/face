cb0a20e37710a100095616ce17e7189edf8655b3
jm4293 <dlwoals4293@gmail.com>
2025-05-17 16:05:03 +0900

    moving adhd

--- src/util/moving_detect.js
+++ src/util/moving_detect.js
@@ -3,6 +3,8 @@ import * as faceapi from "@vladmandic/face-api";
 export async function movingDetect(videoRef, prevBoxRef, alertedRef, intervalIdRef, showModal) {
   await faceapi.nets.tinyFaceDetector.loadFromUri("https://justadudewhohacks.github.io/face-api.js/models");
 
+  let moveCount = 0; // 내부에서 카운트 관리
+
   if (videoRef.current) {
     const video = videoRef.current;
 
@@ -19,8 +21,15 @@ export async function movingDetect(videoRef, prevBoxRef, alertedRef, intervalIdR
 
           if (dist > 50 && !alertedRef.current) {
             alertedRef.current = true;
-
             showModal("움직이네?");
+
+            // 내부 카운트 증가
+            moveCount += 1;
+
+            // 2번 이상이면 리다이렉트
+            if (moveCount >= 2) {
+              window.location.href = "https://www.adhd.or.kr/search/search01.php";
+            }
 
             setTimeout(() => {
               alertedRef.current = false;
