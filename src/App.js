1baa2e529b7a5965ad7f0e8d2c33454f391971f9
jm4293 <dlwoals4293@gmail.com>
2025-05-17 15:30:56 +0900

    dfjfjksfsnkjds

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import * as faceapi from "@vladmandic/face-api";
+import React, { useRef, useEffect } from "react";
 
 function App() {
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    async function setup() {
+      // 모델 로드
+      await faceapi.nets.tinyFaceDetector.loadFromUri("https://justadudewhohacks.github.io/face-api.js/models");
+      // 웹캠 연결
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      if (videoRef.current) videoRef.current.srcObject = stream;
+    }
+    setup();
+  }, []);
+
+  useEffect(() => {
+    async function detect() {
+      if (videoRef.current && canvasRef.current) {
+        const video = videoRef.current;
+        const canvas = canvasRef.current;
+        canvas.width = video.width;
+        canvas.height = video.height;
+
+        setInterval(async () => {
+          const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions());
+          // faceapi.draw.drawDetections(canvas, detections);
+
+          console.log("detections", detections);
+        }, 100);
+      }
+    }
+    detect();
+  }, []);
+
+  console.log("faceapi.nets.", faceapi.nets);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <video ref={videoRef} autoPlay muted width="640" height="480" />
+      <canvas ref={canvasRef} width="640" height="480" style={{ position: "absolute", left: 0, top: 0 }} />
     </div>
   );
 }
